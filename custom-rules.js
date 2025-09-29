909c55f384cfcbbaee852ed4da8d52b8ab846605
itzXian <xixian.2106@gmail.com>
2025-09-29 23:12:50 +0800

    add override write rules

---
+++ custom-rules.js
@@ -0,0 +1,111 @@
+const ruleProvidersBase = {
+    "type": "http",
+    "format": "text",
+    "interval": "3600",
+};
+const ruleProviders = {
+  MIUI_Bloatware : {
+    ...ruleProvidersBase,
+    "behavior": "classical",
+    "url": "https://raw.githubusercontent.com/itzXian/C.C./refs/heads/master/MIUI_Bloatware.list",
+    "path": "./MIUI_Bloatware.yaml"
+  },
+  Block : {
+    ...ruleProvidersBase,
+    "behavior": "classical",
+    "url" : "https://raw.githubusercontent.com/itzXian/C.C./refs/heads/master/Block.list",
+    "path" : "./Block.list"
+  },
+  BanAD : {
+    ...ruleProvidersBase,
+    "behavior": "classical",
+    "url" : "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/BanAD.list",
+    "path" : "./BanAD.list"
+  },
+  BanEasyList : {
+    ...ruleProvidersBase,
+    "behavior": "classical",
+    "url" : "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/BanEasyList.list",
+    "path" : "./BanEasyList.list"
+  },
+  BanEasyListChina : {
+    ...ruleProvidersBase,
+    "behavior": "classical",
+    "url" : "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/BanEasyListChina.list",
+    "path" : "./BanEasyListChina.list"
+  },
+  BanEasyPrivacy : {
+    ...ruleProvidersBase,
+    "behavior": "classical",
+    "url" : "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/BanEasyPrivacy.list",
+    "path" : "./BanEasyPrivacy.list"
+  },
+  BanProgramAD : {
+    ...ruleProvidersBase,
+    "behavior": "classical",
+    "url" : "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/BanProgramAD.list",
+    "path" : "./BanProgramAD.list"
+  },
+  JP : {
+    ...ruleProvidersBase,
+    "behavior": "classical",
+    "url" : "https://raw.githubusercontent.com/itzXian/C.C./refs/heads/master/JP.list",
+    "path" : "./JP.list"
+  },
+  Proxy : {
+    ...ruleProvidersBase,
+    "behavior": "classical",
+    "url" : "https://raw.githubusercontent.com/itzXian/C.C./refs/heads/master/Proxy.list",
+    "path" : "./Proxy.list"
+  }
+}
+
+const prependProxyGroups = [
+  {
+    "name" : "MIUI_BLOATWARE",
+    "type" : "select",
+    "proxies" : [ "REJECT", "DIRECT" ]
+  },
+  {
+    "name" : "AD_BLOCK",
+    "type" : "select",
+    "proxies" : [ "REJECT", "DIRECT" ]
+  },
+  {
+    "name" : "JP_DOMAIN",
+    "type" : "select",
+    "include-all": true,
+    "filter" : "JP|日本",
+    "proxies" : [ "🚀 手动切换", "♻️ 自动选择", "🇯🇵 日本", "DIRECT" ]
+  },
+  {
+    "name" : "PROXY",
+    "type" : "select",
+    "proxies" : [ "🚀 手动切换", "♻️ 自动选择", "REJECT", "DIRECT" ]
+  },
+];
+const prependRule = [
+  "DOMAIN-SUFFIX,clashverge.dev,♻️ 自动选择",
+  "RULE-SET,MIUI_Bloatware,MIUI_BLOATWARE",
+  "RULE-SET,Block,AD_BLOCK",
+  "RULE-SET,BanAD,AD_BLOCK",
+  "RULE-SET,BanEasyList,AD_BLOCK",
+  "RULE-SET,BanEasyListChina,AD_BLOCK",
+  "RULE-SET,BanEasyPrivacy,AD_BLOCK",
+  "RULE-SET,BanProgramAD,AD_BLOCK",
+  "RULE-SET,JP,JP_DOMAIN",
+  "RULE-SET,Proxy,PROXY",
+];
+function main(config) {
+  let oldRules = config["rules"];
+  config["rules"] = prependRule.concat(oldRules);
+
+  let oldProxyGroups = config["proxy-groups"];
+  config["proxy-groups"] = prependProxyGroups.concat(oldProxyGroups);
+
+  Object.assign(config, {
+    "rule-providers": ruleProviders
+  });
+
+  return config;
+}
