3c491af0c9c99fefc6084c8118cd29b3f887ce65
itzXian <xixian.2106@gmail.com>
2026-03-31 21:25:47 +0800

    add a new rule

--- custom.js
+++ custom.js
@@ -233,6 +233,7 @@ const overrideRules = (config) => {
         "GEOSITE,       apple,              APPLE",
         "GEOSITE,       apple-intelligence, APPLE",
         "DOMAIN-SUFFIX, hinative.com,       NON_JP",
+        "DOMAIN-SUFFIX, game8.jp,           NON_JP",
         "DOMAIN-REGEX,  .*\.jp,             JP",
         "GEOIP,         JP,                 JP,              no-resolve",
         "GEOSITE,       geolocation-!cn,    NON_CN",
