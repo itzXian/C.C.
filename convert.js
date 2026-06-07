8a123e05bb2f3ff63b9770fa9b25277a4ffad533
itzXian <xixian.2106@gmail.com>
2026-06-07 11:17:10 +0800

    format

--- convert.js
+++ convert.js
@@ -1,11 +1,11 @@
-const YAML = require('yaml')
-const FS   = require('fs')
+const YAML = require('yaml');
+const FS   = require('fs');
 
-const Custom = require('./custom.js')
-let   Config = YAML.parse(FS.readFileSync('./conf.yaml', 'utf8'))
+const Custom = require('./custom.js');
+let   Config = YAML.parse(FS.readFileSync('./conf.yaml', 'utf8'));
 Custom.main(Config);
 
-FS.writeFile('./new.yaml', YAML.stringify(Config), (err, data) => {
+FS.writeFile('./new.yaml', YAML.stringify(Config), (err) => {
     if (err) {
         console.log(err);
     } else {
