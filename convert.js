575738e0e3038f88316c8c94c292765983533f5c
itzXian <xixian.2106@gmail.com>
2026-06-07 11:11:03 +0800

    update convert.js

--- convert.js
+++ convert.js
@@ -1,19 +1,14 @@
-const oldConfigFile = "./conf.yaml"
-const overrideScript = "./custom.js"
-const newConfigFile = "./new.yaml"
-
 const YAML = require('yaml')
-const fs = require('fs')
+const FS   = require('fs')
 
-const file = fs.readFileSync(oldConfigFile, 'utf8')
-let config = YAML.parse(file)
+const Custom = require('./custom.js')
+let   Config = YAML.parse(FS.readFileSync('./conf.yaml', 'utf8'))
+Custom.main(Config);
 
-const Test = require(overrideScript)
-Test.main(config);
-
-const yamlString = YAML.stringify(config);
-
-fs.writeFile(newConfigFile, yamlString, (err, data) => {
-  if(err) console.log(err);
-  else console.log('write end');
+FS.writeFile('./new.yaml', YAML.stringify(Config), (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('write end');
+    };
 });
